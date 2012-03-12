e9c1112cce226de9b06250fd8ec48d181410050a
JP <jpellerin@leapfrogonline.com>
2012-03-12 17:15:00 -0400

    Move all hooks to runner

--- djnose2/runner.py
+++ djnose2/runner.py
@@ -9,19 +9,18 @@ from nose2.main import PluggableTestProgram
 
 class TestRunner(simple.DjangoTestSuiteRunner):
 
-    default_plugins = [
-        'djnose2.plugins.result',
-        ]
+    err_count = 0
+    _hooks = ['startTestRun', 'reportFailure', 'reportError']
+
+    def hooks(self):
+        return [(hook, self) for hook in self._hooks]
 
     def run_tests(self, test_labels, extra_tests=None, **kwargs):
         self.extra_tests = extra_tests
         with self.test_environment():
             argv = self.make_argv(test_labels)
-            proc = TestProgram(argv=argv, module=None, exit=False,
-                               plugins=self.default_plugins,
-                               hooks=[('startTestRun', self)])
-            stor = getattr(proc.session, 'djnose2_result', {})
-            return stor.get('failed', 0) + stor.get('error', 0)
+            TestProgram(argv=argv, module=None, exit=False, hooks=self.hooks())
+            return self.err_count
 
     def make_argv(self, test_labels):
         argv = ['nose2']
@@ -55,6 +54,12 @@ class TestRunner(simple.DjangoTestSuiteRunner):
             return
         for test in self.extra_tests:
             event.suite.addTest(test)
+
+    def reportFailure(self, event):
+        self.err_count += 1
+
+    def reportError(self, event):
+        self.reportFailure(event)
 
 
 class TestProgram(PluggableTestProgram):
